e54408c3d3eee9ff6b0413c99f3ee206651847dd
Lucas Timoteo da Silva <lucastimoteo90@gmail.com>
2019-03-27 19:17:48 -0300

    att

---
+++ README.md
@@ -0,0 +1,2 @@
+# cleangame
+# cleangame2
---
+++ src/main/java/com/demo/dto/TipDTO.java
@@ -0,0 +1,21 @@
+package com.demo.dto;
+
+public class TipDTO {
+	private Integer question_id;
+	private String tip;
+	
+	public Integer getQuestion_id() {
+		return question_id;
+	}
+	public void setQuestion_id(Integer question_id) {
+		this.question_id = question_id;
+	}
+	public String getTip() {
+		return tip;
+	}
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+
+
+}
